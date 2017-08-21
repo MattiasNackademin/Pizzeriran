37771c2fc55a6b0f7420ca0ba3a1c42c50e92770
mattias sjöden <mattias sjden>
2017-08-21 10:26:44 +0200

    Add project files.

---
+++ InMemD8/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InMemD8.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace InMemD8.Data
+{
+    public class DbInitializer
+    {
+
+        public static void Initialize(UserManager<ApplicationUser> userManager)
+        {
+            var aUser = new ApplicationUser();
+            aUser.UserName = "student@test.com";
+            aUser.Email = "student@test.com";
+            var r = userManager.CreateAsync(aUser, "Pa$$w0rd").Result;
+        }
+    }
+}
