a2d25da88a147829a9196db36dc5ac77334f780a
mattias sjöden <mattias.work@outlook.com>
2017-08-22 16:01:59 +0200

    add Category

---
+++ InMemD8/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace InMemD8.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Dish> Dishes { get; set; }
+    }
+}
--- InMemD8/Models/DishIngredient.cs
+++ InMemD8/Models/DishIngredient.cs
@@ -15,5 +15,6 @@ namespace InMemD8.Models
 
         //test
 
+
     }
 }
