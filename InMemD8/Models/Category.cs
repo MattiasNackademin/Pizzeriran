6d8d2dd60c6c6c249d231413d6907589a32e14b1
mattias sjöden <mattias.work@outlook.com>
2017-09-04 15:17:18 +0200

    adding dishes and ingedient..building cart

--- InMemD8/Models/Category.cs
+++ InMemD8/Models/Category.cs
@@ -9,8 +9,8 @@ namespace InMemD8.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; }
+        
         public List<Dish> Dishes { get; set; }
     }
 }
